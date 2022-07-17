fa5b13ec72f7e60fc4d5034fc9890bcb3ee5573c
devihall <devis0306@gmail.com>
2022-07-17 19:39:15 -0400

    display key value pair

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,49 @@
-//DOM element references
+//////////////////DOM element references///////////////////
 var pEL = document.querySelector("#currentDate")
 var currentDate = document.querySelector("#currentDate")
+var currentCity = document.querySelector("#current-city");
 
-//current date
+
+////////////////////current date///////////////////////////
 var currentDate = moment().format("MMM Do YY");
 pEL.textContent = currentDate
 
 
 
-//grab user input and pass as a query parameter
-document.getElementById("search-btn").addEventListener("click", function(){
-    var cityInput = document.querySelector(".form-control").value;
-var apiKey = "a77950eae898ecd8c88501ac3b12b6b6";
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=" + apiKey;
-// console.log(queryUrl)
-fetch(queryUrl).then(function(response){
-    // console.log(response)
-    if (response.ok){
-        response.json().then(function(data){
-            console.log(data)
-        })
-    }
-});
-})
+//////////grab user input and pass as a query parameter///////
+document.getElementById("search-btn").addEventListener("click", function () {
+  var cityInput = document.querySelector(".form-control").value;
+  var apiKey = "a77950eae898ecd8c88501ac3b12b6b6";
+  var queryUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityInput +
+    "&units=imperial&appid=" +
+    apiKey;
+  // console.log(queryUrl)
 
+  ////////////////////get weather data/////////////////////
+  fetch(queryUrl).then(function (response) {
+    // console.log(response)
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        displayWeatherData(data);
+      });
+    } else {
+      alert("There was a problem");
+    }
+  });
+});
+
+//////////////diplay weather data for searched city//////////////
+var displayWeatherData = function(data){
+var currentTemp = document.createElement("p")
+currentTemp.textContent = response(main.temp)
+console.log(currentTemp)
+
+}
     
-//grab user input and make a list previously searched cities
+//////add user input to a list previously searched cities/////////
  document.getElementById("search-btn").addEventListener("click", function(){
         var cityInput = document.getElementById("input-box").value;
         var searchedCities = document.createElement('li');
@@ -41,24 +59,7 @@ fetch(queryUrl).then(function(response){
 
 
 
-
-
-
-    //   queryUrl = searchCityInput.value()
-
-// event listener on search button
-// searchButtonEl.addEventListener("click", function(event){
-//     searchCityInput = 
-// //  console.log("click")
-// })
-   
-
-
-
-
-
-
 // var storage = localStorage.getItem(search);
 // console.log(storage)
 // search.children[4].value = storage
-// var search = document.querySelector(".gap-2");
+// var search = document.querySelector(".gap-2")
