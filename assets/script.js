cd0ee13a2070bbbfef68b255d2f1a1d3fedf05a4
devihall <devis0306@gmail.com>
2022-07-17 18:17:09 -0400

    user input query parameter

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,50 @@
 //DOM element references
-var searchButtonEl = document.querySelector("#search-btn")
-var inputEl = document.querySelector("#input")
-var cityOptions = document.querySelector("#datalistOptions")
 var pEL = document.querySelector("#currentDate")
-// var currentDate = document.querySelector("#currentDate")
-// var searchValue = input.value
-// console.log(searchButtonEl)
+var currentDate = document.querySelector("#currentDate")
 
-
-
-    var apiKey = "a77950eae898ecd8c88501ac3b12b6b6";
-    var queryUrl =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      cityOptions +
-      "&units=imperial&appid=" + apiKey;
-      console.log(queryUrl)
-    
-
-
-// event listener on search button
-searchButtonEl.addEventListener("click", function(queryUrl){
- console.log("click")
-})
-   
 //current date
 var currentDate = moment().format("MMM Do YY");
-console.log (currentDate)
-// pEl.innerHTML = currentDate
 pEL.textContent = currentDate
+
+
+
+//grab user input and pass as a query parameter
+document.getElementById("search-btn").addEventListener("click", function(){
+    var cityInput = document.querySelector(".form-control").value;
+var apiKey = "a77950eae898ecd8c88501ac3b12b6b6";
+var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=" + apiKey;
+// console.log(queryUrl)
+fetch(queryUrl);
+})
+
+    
+//grab user input and make a list previously searched cities
+ document.getElementById("search-btn").addEventListener("click", function(){
+        var cityInput = document.getElementById("input-box").value;
+        var searchedCities = document.createElement('li');
+        searchedCities.textContent = cityInput;
+        searchedCities.className = "city-ul";
+        document.getElementById("cities").append(searchedCities)
+    })
+
+   
+    
+        
+
+
+
+
+
+
+
+    //   queryUrl = searchCityInput.value()
+
+// event listener on search button
+// searchButtonEl.addEventListener("click", function(event){
+//     searchCityInput = 
+// //  console.log("click")
+// })
+   
 
 
 
